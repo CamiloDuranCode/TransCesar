c008b93925f9ad4ce58ae226c3425a25205eca32
WILBERTO ARIZA ZAPATA <warizaz@unicesar.edu.co>
2026-03-15 17:16:29 -0500

    feat: agregar subclase MicroBus

---
+++ src/main/java/transcesar/model/MicroBus.java
@@ -0,0 +1,31 @@
+package transcesar.model;
+
+public class MicroBus extends Vehiculo implements Imprimible{
+
+    private final double TARIFA = 10000;
+
+    public MicroBus (String placa, String ruta){
+        super(placa, ruta, 25);
+    }
+
+    @Override
+    public double tarifabase() {
+        return TARIFA;
+    }
+
+    @Override
+    public String getTipoVehiculo() {
+        return "MicroBus";
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("Tipo: MicroBus");
+        System.out.println("Placa: " + placa);
+        System.out.println("Ruta: " + ruta);
+        System.out.println("Capacidad máxima: " + capacidadMaxima);
+        System.out.println("Pasajeros actuales: " + contadorPasajeros);
+        System.out.println("Tarifa base: $" + TARIFA);
+
+    }
+}
