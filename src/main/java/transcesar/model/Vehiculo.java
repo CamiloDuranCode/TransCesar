8d5ab5f6598c5a7e6183bdb5ac54574c522351b1
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-03-21 17:56:12 -0500

    feat: implementar Imprimible en Vehiculo y agregar manejo de cupos

--- src/main/java/transcesar/model/Vehiculo.java
+++ src/main/java/transcesar/model/Vehiculo.java
@@ -1,6 +1,6 @@
 package transcesar.model;
 
-public abstract class Vehiculo {
+public abstract class Vehiculo implements Imprimible {
     protected String placa;
     protected Ruta ruta;
     protected int capacidadMaxima;
@@ -8,24 +8,67 @@ public abstract class Vehiculo {
     protected boolean estado;
 
     public Vehiculo(String placa, Ruta ruta, int capacidadMaxima, boolean estado) {
-        this.placa             = placa;
-        this.ruta              = ruta;
-        this.capacidadMaxima   = capacidadMaxima;
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
         this.contadorPasajeros = 0;
-        this.estado            = estado;
+        this.estado = estado;
     }
 
     public abstract double tarifabase();
     public abstract String getTipoVehiculo();
 
-    public String getPlaca()             { return placa;             }
-    public Ruta getRuta()                { return ruta;              }
-    public int getCapacidadMaxima()      { return capacidadMaxima;   }
-    public int getContadorPasajeros()    { return contadorPasajeros; }
-    public boolean getEstado()           { return estado;            }
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public Ruta getRuta() {
+        return ruta;
+    }
 
-    public void setRuta(Ruta ruta)       { this.ruta   = ruta;   }
-    public void setEstado(boolean estado){ this.estado = estado;  }
+    public void setRuta(Ruta ruta) {
+        this.ruta   = ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getContadorPasajeros() {
+        return contadorPasajeros;
+    }
+
+    public void setContadorPasajeros(int contadorPasajeros) {
+        this.contadorPasajeros = contadorPasajeros;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public abstract void imprimirDetalle();
+
+    public void incrementarPasajeros() {
+        this.contadorPasajeros++;
+    }
+
+    public void decrementarPasajeros() {
+        if (contadorPasajeros > 0) this.contadorPasajeros--;
+    }
+
+    public boolean tieneCupos() {
+        return contadorPasajeros < capacidadMaxima;
+    }
 }
 
 
