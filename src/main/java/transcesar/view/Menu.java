ce5ee4d41e6339e4ca1cc9cad43ef30329c99974
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-03-21 19:42:10 -0500

    feat: agregar RutaService

--- src/main/java/transcesar/view/Menu.java
+++ src/main/java/transcesar/view/Menu.java
@@ -15,6 +15,7 @@ public class Menu {
             System.out.println("4. Consultas y Estadísticas");
             System.out.println("5. Reportes");
             System.out.println("6. Reservas");
+            System.out.println("7. Gestión de Rutas");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = scanner.nextInt();
